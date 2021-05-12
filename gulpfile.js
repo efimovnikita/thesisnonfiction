bb47ea4f00012c5ac234cea04a2d06b02610037a
Nikita Efimov <maskedballmail@gmail.com>
2021-05-12 21:39:12 +0300

    инлайнит скрипты в html

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,8 @@ const fs = require("fs");
 const replace = require("gulp-replace");
 const gulp = require("gulp");
 const cleancss = require("gulp-clean-css");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
 
 // Styles
 
@@ -25,10 +27,47 @@ gulp.task("styles:inline", () => {
     .pipe(gulp.dest("dist"));
 });
 
+// Scripts
+
+gulp.task("scripts:compress", () => {
+  return gulp
+    .src("src/scripts/scripts.js")
+    .pipe(
+      babel({
+        presets: ["@babel/preset-env"],
+      })
+    )
+    .pipe(uglify())
+    .pipe(gulp.dest("dist"));
+});
+
+gulp.task("scripts:inline", () => {
+  return gulp
+    .src("dist/**/*.html")
+    .pipe(
+      replace(/<script src="\/scripts\/scripts.js"><\/script>/, () => {
+        const style = fs.readFileSync("dist/scripts.js", "utf8");
+        return "<script>" + style + "</script>";
+      })
+    )
+    .pipe(gulp.dest("dist"));
+});
+
+// Clean
+
 gulp.task("clean", () => {
-  return del("dist/styles");
+  return del(["dist/styles", "dist/scripts", "dist/scripts.js"]);
 });
 
 // Build
 
-gulp.task("build", gulp.series("minify-styles", "styles:inline", "clean"));
+gulp.task(
+  "build",
+  gulp.series(
+    "minify-styles",
+    "styles:inline",
+    "scripts:compress",
+    "scripts:inline",
+    "clean"
+  )
+);
