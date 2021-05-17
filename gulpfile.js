0775c01b86c145976cada54db8c6247ab20985c0
Ефимов Никита Сергеевич <Efimov@TSDomain.ru>
2021-05-17 10:31:31 +0300

    вносит небольшие изменения в gulp-файл

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,4 @@
 const del = require("del");
-const fs = require("fs");
-const replace = require("gulp-replace");
 const gulp = require("gulp");
 const cleancss = require("gulp-clean-css");
 const babel = require("gulp-babel");
@@ -15,18 +13,6 @@ gulp.task("minify-styles", () => {
     .pipe(gulp.dest("dist/styles"));
 });
 
-// gulp.task("styles:inline", () => {
-//   return gulp
-//     .src("dist/**/*.html")
-//     .pipe(
-//       replace(/<link rel="stylesheet" href="\/styles\/style.css">/, () => {
-//         const style = fs.readFileSync("dist/styles/style.css", "utf8");
-//         return "<style>" + style + "</style>";
-//       })
-//     )
-//     .pipe(gulp.dest("dist"));
-// });
-
 // Scripts
 
 gulp.task("scripts:compress", () => {
@@ -38,36 +24,15 @@ gulp.task("scripts:compress", () => {
       })
     )
     .pipe(uglify())
-    .pipe(gulp.dest("dist"));
+    .pipe(gulp.dest("dist/scripts"));
 });
-
-// gulp.task("scripts:inline", () => {
-//   return gulp
-//     .src("dist/**/*.html")
-//     .pipe(
-//       replace(/<script src="\/scripts\/scripts.js"><\/script>/, () => {
-//         const style = fs.readFileSync("dist/scripts.js", "utf8");
-//         return "<script>" + style + "</script>";
-//       })
-//     )
-//     .pipe(gulp.dest("dist"));
-// });
 
 // Clean
 
-// gulp.task("clean", () => {
-//   return del(["dist/styles", "dist/scripts", "dist/scripts.js"]);
-// });
+gulp.task("clean", () => {
+  return del("dist/styles/common.blocks");
+});
 
 // Build
 
-gulp.task(
-  "build",
-  gulp.series(
-    "minify-styles",
-    // "styles:inline",
-    "scripts:compress",
-    // "scripts:inline",
-    // "clean"
-  )
-);
+gulp.task("build", gulp.series("minify-styles", "scripts:compress", "clean"));
